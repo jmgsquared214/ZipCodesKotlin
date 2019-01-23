afd682a7edae52813dd0af0241aa29a25932b9a1
Jamie Gammel <jmgsquared214@gmail.com>
2019-01-23 15:19:29 -0700

    adding README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Create a file containing US zipcodes (at least a variety)
+
+Read in the file of zip codes
+
+Process the ones that begin with 8.
+
+Multiple each by 1.5
+
+For each of the results – print the sorted list.
+
+Add unit tests for functionality
--- src/main/ZipCode.kt
+++ src/main/ZipCode.kt
@@ -1,13 +1,11 @@
-package main
 
-fun main (args: Array<String>) {
 
-val zipCount = mutableMapOfString<Int, Int>()
-Read in the file of zip codes
 
-File("src/zipcodes.txt").forEachLine {
-val previous = zipCount.getOrDefault(it, 0)
-zipCount.put(it, previous + 1)
-}
+fun main(args: Array<String>) {
 
+    val ipToCount = mutableMapOf<String, Int>()
+    File("src/zipcodes.txt").forEachLine {
+        val previous = ipToCount.getOrDefault(it, 0)
+        ipToCount.put(it, previous + 1)
+    }
 }
