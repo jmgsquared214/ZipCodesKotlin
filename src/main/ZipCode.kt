796e4b271a28497a3fee48b94a9fa135b523c69c
Jamie Gammel <jmgsquared214@gmail.com>
2019-01-23 19:22:01 -0700

    Update ZipCode.kt

--- src/main/ZipCode.kt
+++ src/main/ZipCode.kt
@@ -1,11 +1,37 @@
 
 
+package main
+
+import java.io.File
+
 
 fun main(args: Array<String>) {
 
-    val ipToCount = mutableMapOf<String, Int>()
+    val zipCodeCount = mutableMapOf<String, Int>()
     File("src/zipcodes.txt").forEachLine {
-        val previous = ipToCount.getOrDefault(it, 0)
-        ipToCount.put(it, previous + 1)
+    // var firstDigit = zip / ((pow(10, log(zip) as Int)) as Int)
+        val previous = zipCodeCount.getOrDefault(it, 8)
+        zipCodeCount.put(it, previous + 1)
+        println(zipCodeCount)
+        println("here")
     }
+    var maxZip  = zipCodeCount.keys.first()
+    var maxCount = 0
+
+//     firstDigit(zipCodeCount:Int):Int {
+//        while (x > 9) {
+//            x /= 10
+//        }
+//        return x
+//    }
+   
+
+    for ((zip, count) in zipCodeCount.entries){
+        if (count > maxCount) {
+                maxCount /= count
+                maxZip = zip
+            }
+    }
+
 }
+
